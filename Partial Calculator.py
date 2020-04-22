9848611a90647da9938cd5adebbb2a525d6b3785
amirsal1 <63913916+amirsal1@users.noreply.github.com>
2020-04-22 23:54:15 +0300

    Update Partial Calculator.py

--- Partial Calculator.py
+++ Partial Calculator.py
@@ -18,14 +18,14 @@ while final_word != word:
             remainder_cover = float(input("When did you cover remainder?"))
             remainder_profit = (remainder_cover - Executed_trigger) * 0.25 * Shares
             print(remainder_profit)
+            print("Overall trade profit: " + str(first_partial_profit + second_partial_profit + remainder_profit))
         else:
             print(str((Trigger-Executed_trigger)*0.5*Shares))
     else:
         Executed_stop = float(input("When did Stop execute?"))
         loss = abs(Executed_trigger - Executed_stop) * Shares
         print("You lost " + str(loss))
-    print(" ")
-    print(" ")
+    print("\n\n")
     print("Taking partials 2:1 50% 50%")
     first_partial = input("Did you reach your first target of " + str(((Trigger - Stop) * 2 + Trigger)) + str("?"))
     if first_partial == "True":
@@ -35,14 +35,14 @@ while final_word != word:
         if second_partial == "True":
             second_partial_profit = (((Trigger - Stop) * 4 + Trigger) - Executed_trigger) * 0.5 * Shares
             print(str(second_partial_profit))
+            print("Overall trade profit: " + str(first_partial_profit + second_partial_profit))
         else:
            print(str((Trigger - Executed_trigger)*0.5*Shares))
     else:
         Executed_stop = float(input("When did Stop execute?"))
         loss = abs(Executed_trigger - Executed_stop) * Shares
         print("You lost " + str(loss))
-    print(" ")
-    print(" ")
+    print("\n\n")
     print("Taking partials 1:1 75% 25%")
     first_partial = input("Did you reach your first target of " + str(((Trigger - Stop) + Trigger)) + str("?"))
     if first_partial == "True":
@@ -51,6 +51,17 @@ while final_word != word:
         remainder_cover = float(input("Where did you cover remainder? "))
         remainder_profit = (remainder_cover - Executed_trigger)*0.25*Shares
         print(str(remainder_profit))
+        print("Overall trade profit: " + str(first_partial_profit + remainder_profit))
+    else:
+        Executed_stop = float(input("When did Stop execute?"))
+        loss = abs(Executed_trigger - Executed_stop) * Shares
+        print("You lost " + str(loss))
+    print("\n\n")
+    print("Taking partials 1:1 100%")
+    first_partial = input("Did you reach your target of " + str(((Trigger - Stop) + Trigger)) + str("?"))
+    if first_partial == "True":
+        target_profit = (((Trigger - Stop) + Trigger) - Executed_trigger) * Shares
+        print(str(target_profit))
     else:
         Executed_stop = float(input("When did Stop execute?"))
         loss = abs(Executed_trigger - Executed_stop) * Shares
